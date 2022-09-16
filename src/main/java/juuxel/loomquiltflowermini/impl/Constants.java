7d04f3202383822d810a2057b79eaa2343bd5ff8
Cephetir <62209929+Cephetir@users.noreply.github.com>
2022-09-16 19:33:42 +0300

    e

--- src/main/java/juuxel/loomquiltflowermini/impl/Constants.java
+++ src/main/java/juuxel/loomquiltflowermini/impl/Constants.java
@@ -2,5 +2,5 @@ package juuxel.loomquiltflowermini.impl;
 
 public final class Constants {
     public static final String QUILT_MAVEN = "https://maven.quiltmc.org/repository/release";
-    public static final String QUILTFLOWER_DEPENDENCY = "org.quiltmc:quiltflower:1.6.0";
+    public static final String QUILTFLOWER_DEPENDENCY = "org.quiltmc:quiltflower:1.8.0";
 }
